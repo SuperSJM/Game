f310eb90e21079313a0d04fed61f615e62f959da
SuperSJM <1376169300@qq.com>
2021-01-18 12:53:22 +0800

    first commit

---
+++ New Unity Project/Assets/Music.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private static Music instance = null;
+    public static Music Instance
+    {
+        get { return instance; }
+    }
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);//使对象目标在加载新场景时不被自动销毁。
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
